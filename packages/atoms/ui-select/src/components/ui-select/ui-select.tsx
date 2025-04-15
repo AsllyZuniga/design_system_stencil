b098a075b9bd3b5049c7fc265ac45c9375c7a821
Aslly Zuñiga <asllyz2610@gmail.com>
2025-04-15 17:47:06 -0500

    Created ui select

---
+++ packages/atoms/ui-select/src/components/ui-select/ui-select.tsx
@@ -0,0 +1,16 @@
+import { Component, Host, h, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'ui-select',
+  styleUrl: 'ui-select.scss',
+  shadow: true,
+})
+export class UiSelect {
+  render() {
+    return (
+      <Host>
+      
+      </Host>
+    );
+  }
+}
