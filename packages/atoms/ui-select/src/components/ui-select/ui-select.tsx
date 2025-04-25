47e4e0b6f927b2eb5d53f0a1ca6ad95a99fdbb32
Aslly Zuñiga <asllyz2610@gmail.com>
2025-04-25 14:20:37 -0500

    modidied ui-select

--- packages/atoms/ui-select/src/components/ui-select/ui-select.tsx
+++ packages/atoms/ui-select/src/components/ui-select/ui-select.tsx
@@ -1,15 +1,40 @@
-import { Component, Host, h, Prop } from '@stencil/core';
+import { Component, Host, h, Prop } from "@stencil/core";
 
 @Component({
-  tag: 'ui-select',
-  styleUrl: 'ui-select.scss',
+  tag: "ui-select",
+  styleUrl: "ui-select.scss",
   shadow: true,
 })
 export class UiSelect {
+  @Prop() autofocus: boolean = false;
+  @Prop() disabled: boolean = false;
+  @Prop() form?: string;
+  @Prop() multiple: boolean = false;
+  @Prop() name?: string;
+  @Prop() required: boolean = false;
+  @Prop() size?: number;
+
   render() {
     return (
       <Host>
-      
+        <label>
+          Movimiento
+          <select
+            autofocus={this.autofocus}
+            disabled={this.disabled}
+            form={this.form}
+            multiple={this.multiple}
+            name={this.name}
+            required={this.required}
+            size={this.size}
+          >
+            <option value="">Seleccione</option>
+            <option value="pago">Pago</option>
+            <option value="gasto">Gasto</option>
+            <option value="inversion">Inversión</option>
+            <option value="prestamo">Préstamo</option>
+          </select>
+        </label>
       </Host>
     );
   }
