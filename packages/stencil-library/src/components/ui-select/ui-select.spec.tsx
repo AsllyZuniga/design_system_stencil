0e16c84825fa4ad017fd596f917c65bddaf4cd7c
Aslly Zuñiga <asllyz2610@gmail.com>
2026-04-01 20:46:08 -0500

    refactor(test): standardize component test files to .spec.tsx

---
+++ packages/stencil-library/src/components/ui-select/ui-select.spec.tsx
@@ -0,0 +1,18 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { UiSelect } from './ui-select';
+
+describe('ui-select', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [UiSelect],
+      html: `<ui-select></ui-select>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <ui-select>
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </ui-select>
+    `);
+  });
+});
