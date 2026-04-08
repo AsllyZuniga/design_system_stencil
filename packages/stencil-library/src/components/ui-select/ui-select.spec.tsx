3a81e9ccb5e12d179b882e4f135303c234232b15
Aslly Zuñiga <asllyz2610@gmail.com>
2026-04-08 11:09:47 -0500

    Add test to ensure ui-select does not render ui-input internally

--- packages/stencil-library/src/components/ui-select/ui-select.spec.tsx
+++ packages/stencil-library/src/components/ui-select/ui-select.spec.tsx
@@ -9,4 +9,14 @@ describe('ui-select', () => {
     });
     expect(page.root).toBeTruthy();
   });
+
+  it('does not render ui-input internally', async () => {
+    const page = await newSpecPage({
+      components: [UiSelect],
+      html: `<ui-select label="Año"></ui-select>`,
+    });
+
+    expect(page.root?.shadowRoot?.querySelector('ui-input')).toBeNull();
+    expect(page.root?.shadowRoot?.querySelector('.ui-select-trigger')).toBeTruthy();
+  });
 });
